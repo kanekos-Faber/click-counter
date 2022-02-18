25f06a8f6de2fe86c1cf4aeea778b736b3d92736
kanekos-Faber <kaneko@fabercompany.co.jp>
2022-02-18 17:35:22 +0900

    Manage user ID on session

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const session = require("express-session");
 const app = express();
 const axios = require("axios");
 const cors = require('cors');
@@ -10,16 +11,29 @@ const corsOptions = {
     optionsSuccessStatus: 200,
 }
 
+app.use(session({
+    secret: 'thisissecretofclickcounter',
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+        maxage: 1000 * 60 * 30
+    }
+}));
+
 app.use(cors(corsOptions));
 
 var jsonParser = bodyparser.json()
 // var urlencodedParser = bodyparser.urlencoded({ extended: false })
 
 app.post("/", jsonParser, function (req, res) {
-    console.log(req.body.session_id);
-    if (req.body.session_id === undefined || !req.body.session_id) {
-        res.send('Not authorized!\n');
-        return;
+    console.log(req.session.userId);
+    if (req.session.userId == undefined) {
+        //TODO: Store userId into DynamoDB
+        req.session.userId = req.session.id;
+    } else {
+        //TODO: Validate the userId is active and not reached limit
     }
     axios
         .get("https://jsonplaceholder.typicode.com/todos/1")
